6d35d22d5930d25a51e1613fbda9f4f14f39e881
toshota <toshota@student.42tokyo.jp>
2024-01-23 20:02:45 +0900

    use is_even(); instead of is_odd();

--- philo/srcs/do_philo/do_philo.c
+++ philo/srcs/do_philo/do_philo.c
@@ -6,22 +6,22 @@
 /*   By: toshota <toshota@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 16:41:35 by toshota           #+#    #+#             */
-/*   Updated: 2024/01/23 19:57:30 by toshota          ###   ########.fr       */
+/*   Updated: 2024/01/23 20:01:48 by toshota          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_func.h"
 
-static bool	is_odd(int num)
+static bool	is_even(int num)
 {
-	return (num % 2 == 1);
+	return (num % 2 == 0);
 }
 
 void	*do_philo(t_philo *philo)
 {
 	if (sleep_until_all_threads_are_created(philo) == false)
 		return (NULL);
-	if (!is_odd(philo->philo_nb))
+	if (is_even(philo->philo_nb))
 		ft_usleep((philo->common->time_to_eat / 2) * 1000);
 	while (true)
 	{
